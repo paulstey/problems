c2c8e45d17343b5bb028c73d906cc4647c574731
paulstey <paulstey@gmail.com>
2017-04-29 07:23:07 -0400

    first commit

---
+++ src/prob3/prob3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+
+std::vector<int> primes(int n) {
+    std::vector<int> nums(n);
+    std::iota(n.begin(), n.end(), 1);
+
+    nums[0] = -1
+    nums[1] = -1
+    
